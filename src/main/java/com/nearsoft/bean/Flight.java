6de734809317ada423daf7c661f94e93a105b0ce
Saidel Lopez <slopez@nearsoft.com>
2014-03-20 17:20:55 -0700

    revamped bean class now without notations for hibernate. They won't be in database anymore. This new version only includes what we need in web

--- src/main/java/com/nearsoft/bean/Flight.java
+++ src/main/java/com/nearsoft/bean/Flight.java
@@ -1,124 +1,140 @@
 package com.nearsoft.bean;
 
 
-import com.sun.istack.internal.NotNull;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.hibernate.validator.constraints.NotBlank;
 
 import javax.persistence.*;
-import java.sql.Timestamp;
-import java.util.Set;
+import java.io.Serializable;
 
 /**
  * Created by slopez on 2/27/14.
  */
-@Entity
-@javax.persistence.Table(name="flights")
-@Inheritance(strategy= InheritanceType.SINGLE_TABLE)
-public class Flight {
+public class Flight implements Serializable {
 
+    @JsonIgnore
+    private Long id ;
+    private String price;
+    private String type;
+    //@JsonProperty("est") if change the name of the attribute is needed
+    private String estimateDate1;
+    private String estimateDate2;
+    private String companies;
+    private String estimateTimeTravel;
+    private String airports;
+    private String stops;
+    private String scales;
+
+    /**
+     * Sole constructor
+     */
     public Flight(){}
 
-    public Flight(Long id, String price, Timestamp estimateDate1, Timestamp estimateDate2, Set<Company> companies,
-                  String estimateTimeTravel, Set<Airport> airports, String stops, String scales) {
-        this.id = id;
-        this.price = price;
-        this.estimateDate1 = estimateDate1;
-        this.estimateDate2 = estimateDate2;
-        this.companies = companies;
-        this.estimateTimeTravel = estimateTimeTravel;
-        this.airports = airports;
-        this.stops = stops;
-        this.scales = scales;
+    /**
+     * Creates an instance of a flight object
+     * @param id unique identifier for a flight
+     * @param price the price of the flight commonly on dollars
+     * @param estimateDate1 the date and hour of departure
+     * @param estimateDate2 the date and hour of arrival (in a round trip this represents the departure time of return flight)
+     * @param companies the airlines involved in the flight
+     * @param estimateTimeTravel the estimated time travel
+     * @param airports the airports involved in the flight (departure, arrival, scales)
+     * @param stops the number of stops/scales to take in the journey
+     * @param scales a description of the stops as convenient of the API interest
+     */
+    public Flight(Long id, String price, String estimateDate1, String estimateDate2, String companies,
+                  String estimateTimeTravel, String airports, String stops, String scales) {
+        this.setId(id);
+        this.setPrice(price);
+        this.setEstimateDate1(estimateDate1);
+        this.setEstimateDate2(estimateDate2);
+        this.setCompanies(companies);
+        this.setEstimateTimeTravel(estimateTimeTravel);
+        this.setAirports(airports);
+        this.setStops(stops);
+        this.setScales(scales);
     }
 
-    @Id
-    @GeneratedValue
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
-    private Long id ;
 
-    @NotNull @Column(updatable=false)
     public String getPrice() {
         return price;
     }
+
     public void setPrice(String price) {
         this.price = price;
     }
-    private String price;
 
-    @NotNull @Column(updatable=false)
     public String getType() {
         return type;
     }
+
     public void setType(String type) {
         this.type = type;
     }
-    private String type;
 
-    @Temporal(TemporalType.TIMESTAMP) @NotNull @Column(updatable=false)
-    public Timestamp getEstimateDate1() {
+    public String getEstimateDate1() {
         return estimateDate1;
     }
-    public void setEstimateDate1(Timestamp estimateDate1) {
+
+    public void setEstimateDate1(String estimateDate1) {
         this.estimateDate1 = estimateDate1;
     }
-    private Timestamp estimateDate1;
 
-    @Temporal(TemporalType.TIMESTAMP) @NotNull @Column(updatable=false)
-    public Timestamp getEstimateDate2() {
+    public String getEstimateDate2() {
         return estimateDate2;
     }
-    public void setEstimateDate2(Timestamp estimateDate2) {
+
+    public void setEstimateDate2(String estimateDate2) {
         this.estimateDate2 = estimateDate2;
     }
-    private Timestamp estimateDate2;
 
-    @ManyToMany(mappedBy = "companies") @OrderBy("name")
-    public Set<Company> getCompanies() {
+    public String getCompanies() {
         return companies;
     }
-    public void setCompanies(Set<Company> companies) {
+
+    public void setCompanies(String companies) {
         this.companies = companies;
     }
-    private Set<Company> companies;
 
-    @NotNull @Column(updatable=false)
     public String getEstimateTimeTravel() {
         return estimateTimeTravel;
     }
+
     public void setEstimateTimeTravel(String estimateTimeTravel) {
         this.estimateTimeTravel = estimateTimeTravel;
     }
-    private String estimateTimeTravel;
 
-    @ManyToMany(mappedBy = "airport") @OrderBy("name")
-    public Set<Airport> getAirports() {
+    public String getAirports() {
         return airports;
     }
-    public void setAirports(Set<Airport> airports) {
+
+    public void setAirports(String airports) {
         this.airports = airports;
     }
-    private Set<Airport> airports;
 
-    @NotNull @Column(updatable=false)
     public String getStops() {
         return stops;
     }
+
     public void setStops(String stops) {
         this.stops = stops;
     }
-    private String stops;
 
-    @NotNull @Column(updatable=false)
     public String getScales() {
         return scales;
     }
+
     public void setScales(String scales) {
         this.scales = scales;
     }
-    private String scales;
-
 }
