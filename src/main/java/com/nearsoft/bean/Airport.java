9d62a0c4ebadc51a165f34479a5dbe9c37428d3b
Saidel Lopez <slopez@nearsoft.com>
2014-03-20 17:25:25 -0700

    relocation of all the methods. Though they sould be in some position in order to be processed by the hibernate notations.

--- src/main/java/com/nearsoft/bean/Airport.java
+++ src/main/java/com/nearsoft/bean/Airport.java
@@ -12,66 +12,76 @@ import javax.persistence.*;
 @Inheritance(strategy= InheritanceType.SINGLE_TABLE)
 public class Airport {
 
-    public Airport(){}
-
-    public Airport(Long id, String name, String iataCode) {
-        this.id = id;
-        this.name = name;
-        this.iataCode = iataCode;
-    }
-    public Airport(Long id, String name, String iataCode, String country, String city) {
-        this.id = id;
-        this.name = name;
-        this.iataCode = iataCode;
-        this.country = country;
-        this.city = city;
-    }
-
     @Id
     @GeneratedValue
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
     private Long id ;
 
     @NotBlank @Column(updatable=false)
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
     private String name;
 
     @NotBlank @Column(updatable=false)
-    public String getIataCode() {
-        return iataCode;
-    }
-    public void setIataCode(String iataCode) {
-        this.iataCode = iataCode;
-    }
     private String iataCode;
 
     @NotBlank @Column(updatable=false)
-    public String getCountry() {
-        return country;
-    }
-    public void setCountry(String country) {
-        this.country = country;
-    }
     private String country;
 
     @NotBlank @Column(updatable=false)
+    private String city;
+
+    public Airport(){}
+
+    public Airport(Long id, String name, String iataCode) {
+        this.setId(id);
+        this.setName(name);
+        this.setIataCode(iataCode);
+    }
+    public Airport(Long id, String name, String iataCode, String country, String city) {
+        this.setId(id);
+        this.setName(name);
+        this.setIataCode(iataCode);
+        this.setCountry(country);
+        this.setCity(city);
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIataCode() {
+        return iataCode;
+    }
+
+    public void setIataCode(String iataCode) {
+        this.iataCode = iataCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
-    private String city;
-
 }
 
