f0d0890945d236b3977e93edc8d838ba759dd0a6
Saidel Lopez <slopez@nearsoft.com>
2014-03-21 17:09:15 -0700

    revamped of this file, introduced new models

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -2,58 +2,55 @@ App = Ember.Application.create();
 
 // put your routes here
 App.Router.map(function() {
-    this.resource('showResults');
+    this.resource('search');
 });
 
-App.IndexRoute = Ember.Route.extend({
+App.SearchRoute = Ember.Route.extend({
     model: function() {
-        return flights;
+        var query = "/flights?from=";
+        query += "" + "&to=";//add from
+        query += "" + "&startDate=";//add to
+        query += "" + "&endDate=";//add startDate
+        query += "" + "&type=";//add endDate
+        query += "";//add type
+        //'/flights?from=&to=&startDate=&endDate=&type='
+        return $.getJSON(query).then(function(data) {
+            return data;
+        });
     }
 });
 
-var flights = [{
-    id : '1',
-    price: '428',
-    type: 'round trip',
-    estimateDate1: '8:45 am',
-    estimateDate2: '10:45 am',
-    companies: ['Aeromexico', 'Delta'],
-    estimateTimeTravel: '11h 00M',
-    airports: ['HMO', 'JFK'],
-    stops: '1 stop',
-    scales: '3h 30m in MEX'
-} , {
-    id : '2',
-    price: '728',
-    type: 'round trip',
-    estimateDate1: '8:45 am',
-    estimateDate2: '11:59 am',
-    companies: ['Delta', 'Aeromexico'],
-    estimateTimeTravel: '12h 14M',
-    airports: ['HMO', 'LGA'],
-    stops: '2 stops',
-    scales: 'MEX, ATL'
-} , {
-    id : '3',
-    price: '606',
-    type: 'round trip',
-    estimateDate1: '17:45 pm',
-    estimateDate2: '8:20 am',
-    companies: ['Aeromexico'],
-    estimateTimeTravel: '11h 35M',
-    airports: ['HMO', 'JFK'],
-    stops: '3 stops',
-    scales: 'CEN, GDL, MEX'
-} , {
-    id : '4',
-    price: '610',
-    type: 'round trip',
-    estimateDate1: '11:40 am',
-    estimateDate2: '12:56 pm',
-    companies: ['US Airways'],
-    estimateTimeTravel: '7h 16m',
-    airports: ['HMO', 'JFK'],
-    stops: 'non-stop',
-    scales: ''
-}];
+App.SearchParameters = Ember.Route.extend({
+    fromV : "",
+    toV : "",
+    startDateV : "",
+    endDateV : "",
+    typeV : ""
+});
 
+App.IndexRoute = Ember.Route.extend({
+    model: function(){
+        return App.SearchParameters.create()
+    },
+    setupController : function(controller, model){
+        controller.set("model", model);
+    }
+});
+
+App.IndexController = Ember.ObjectController.extend({
+    submitAction : function(){
+        var query = "/flights?from=";
+        query += "" + "&to=";//add from
+        query += "" + "&startDate=";//add to
+        query += "" + "&endDate=";//add startDate
+        query += "" + "&type=";//add endDate
+        query += "";//add type
+        //'/flights?from=&to=&startDate=&endDate=&type='
+        return $.getJSON(query).then(function(data) {
+            return data;
+        });
+    },
+    selectType : function() {
+        return data;
+    }
+});
