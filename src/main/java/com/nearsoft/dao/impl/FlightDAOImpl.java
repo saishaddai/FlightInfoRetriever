84c0f06e6b0894f3fda037756dd4ef5baa119661
Saidel Lopez <slopez@nearsoft.com>
2014-03-26 14:33:15 -0700

    @Repository added

--- src/main/java/com/nearsoft/dao/impl/FlightDAOImpl.java
+++ src/main/java/com/nearsoft/dao/impl/FlightDAOImpl.java
@@ -5,9 +5,11 @@ import com.nearsoft.dao.FlightDAO;
 import com.nearsoft.persistence.BaseHibernateDAO;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class FlightDAOImpl extends BaseHibernateDAO<Flight, Integer> implements FlightDAO {
 
     @Autowired
