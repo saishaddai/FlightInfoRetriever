1909bfebabd56f44cd61888eb703accd08e784f3
Saidel Lopez <slopez@nearsoft.com>
2014-03-26 14:06:31 -0700

    API connection testing with headers. Also I put a main method to test a call api of Despegar with a fake string in the header to communicate

--- src/main/java/com/nearsoft/utils/APIConnection.java
+++ src/main/java/com/nearsoft/utils/APIConnection.java
@@ -8,18 +8,26 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.ConnectException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by slopez on 3/11/14.
  */
 public class APIConnection {
 
-    public static Object callAPI(String url) throws ConnectException {
+    public static Object callAPI(String url, Map<String, String> headers) throws ConnectException {
         HttpClient client = HttpClientBuilder.create().build();
         HttpGet request = new HttpGet(url);
-        request.setHeader("X-ApiKey","AG28754");
+
+        if(headers != null) {
+            for(String key : headers.keySet()) {
+                request.setHeader(key, headers.get(key));
+            }
+        }
+
         StringBuilder textView = new StringBuilder();
-        HttpResponse response = null;
+        HttpResponse response;
         try {
             response = client.execute(request);
             // Get the response
@@ -29,9 +37,21 @@ public class APIConnection {
                 textView.append(line);
             }
         } catch (Exception e) {
+            e.printStackTrace();
             throw new ConnectException();
         }
+        System.out.println(textView);
         return textView;
 
+    }
+
+    public static void main(String[] args) {
+        Map<String, String > headers = new HashMap<>();
+        headers.put("X-ApiKey","AG28754");
+        try {
+            APIConnection.callAPI("http://api.despegar.com/availability/flights/oneWay/HMO/LON/2014-05-25/1/0/0", headers);
+        }catch (Exception e) {
+            System.err.println("ERROR ERROR ERROR ERROR ERRRO ERROR ERROR ERROR ERROR ERRRO ERROR ERROR ERROR ERROR ERRRO");
+        }
     }
 }
