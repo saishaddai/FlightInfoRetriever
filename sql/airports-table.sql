716a8e89dc4b6e2acc20f9496b91efbdbf1b74f9
slopez <slopez@nearsoft.com>
2014-04-11 11:40:46 -0400

    the creation of the table airports. It is not necessary to run because hibernate does it but I upload it anyway  as a repository

---
+++ sql/airports-table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE airports (
+    id bigint NOT NULL,
+    city character varying(255) NOT NULL,
+    country character varying(255) NOT NULL,
+    iatacode character varying(255) NOT NULL,
+    isocountry character varying(255) NOT NULL,
+    name character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.airports OWNER TO postgres;
