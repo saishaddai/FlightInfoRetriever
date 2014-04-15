54c628cd598478315481a46063b4d24c2a492cf6
slopez <slopez@nearsoft.com>
2014-04-15 15:16:12 -0400

    barely working system. Frontend

--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -12,53 +12,48 @@
     <link rel="stylesheet" href="/resources/css/mystyle.css">
     <link rel="icon" type="image/x-icon" href="/resources/favicon.ico"/>
     <link rel="stylesheet" href="/resources/js/libs/jquery-ui/css/smoothness/jquery-ui-1.10.4.custom.css">
-    <script type="text/javascript" src="/resources/js/libs/jquery-1.10.2.js"></script>
-    <script type="text/javascript" src="/resources/js/libs/handlebars-1.1.2.js"></script>
-    <script type="text/javascript" src="/resources/js/libs/ember-1.4.0.js"></script>
-    <script type="text/javascript" src="/resources/js/app.js"></script>
-    <script src="/resources/js/libs/jquery-ui/js/jquery-ui-1.10.4.custom.js"></script>
-    <script>
-        $(function () {
-            $("#firstDay").datepicker({ dateFormat: "yy-mm-dd" });
-            $("#secondDay").datepicker({ dateFormat: "yy-mm-dd" });
-        });
-    </script>
+
+
 </head>
 <body>
 
 <script type="text/x-handlebars" data-template-name="application">
-    {{outlet}}
-
-</script>
-
-<script type="text/x-handlebars" data-template-name="index">
+    {{#link-to 'index' tagName='button' class="optionConf" }} Search {{/link-to}}
+    {{#link-to 'bookedFlights' tagName='button' class="optionConf"}} My Flights {{/link-to}}
     <div class="configurationArea">
         <div class="topConfiguration">
             <%--<div class="optionConf"> Round Trip </div>--%>
     <%-- <div class="optionConf"> One Way </div> --%>
-            <button type="button" class="optionConf" id="oneWay" {{action "oneWay"}}>One Way</button>
-            <%-- <button type="button" class="optionConf" id="roundTrip" {{action "roundTrip"}}>Round Trip</button> --%>
+    <%--<button type="button" class="optionConf" id="oneWay" {{action "oneWay"}}>One Way</button>--%>
+    <%-- <button type="button" class="optionConf" id="roundTrip" {{action "roundTrip"}}>Round Trip</button> --%>
         </div>
         <div class="middleConfiguration" >
             <div class="daForm" >
                 <div class="config1">
-                    {{input type="text" valueBinding="fromV"  name="sourceFlight" id="sourceFlight" class="input" size="31" maxlength="255" }} &nbsp;
-                    {{input type="text" valueBinding="toV"  class="input toLeft" name="destinyFlight" id="destinyFlight" size="31" maxlength="255" }}&nbsp;
-                    <input type="text" class="firstDay" id="firstDay">
-                    <input type="text" class="secondDay" id="secondDay">
+                    {{view App.AutoCompleteSourceView}}
+                    <%-- {{input type="text"  value=model.fromV  name="sourceFlight" id="sourceFlight" class="input" size="31" maxlength="255" }} &nbsp; --%>
+
+                    {{view App.AutoCompleteDestinyView}}
+                    <%--{{input type="text"  value=model.toV  class="input toLeft" name="destinyFlight" id="destinyFlight" size="31" maxlength="255" }}&nbsp;--%>
+
+                    {{view App.Date1View}}
+                    <%-- must be attached to model.date1 {{input type="text" value=model.date1 class="firstDay" id="firstDay"}}--%>
+
+                    {{input type="text" value=model.date2 disabled=true class="secondDay" id="secondDay"}}
                 </div>
                 <div class="config2" >
-                    <button class="buttonF" {{action "submitAction"}}>Go</button>
+                <%-- {{#link-to 'search' class="buttonF" tagName="button"}}Go{{/link-to}} --%>
+    <%-- {{#link-to 'search' class="buttonF" tagName="button"}}Go{{/link-to}} --%>
+                    <button class="buttonF" {{action "search" this}}>Go</button>
                 </div>
             </div>
         </div>
     </div>
     {{outlet}}
-
 </script>
 
-
-<script type="text/x-handlebars" data-template-name="flights">
+<script type="text/x-handlebars" data-template-name="search">
+<p>im a misfit</p>
     <div class="resultsArea">
         {{#each}}
         <div class="result even">
@@ -67,7 +62,7 @@
                 <div class="type">{{type}}</div>
             </div>
             <div class="section2">
-                <div class="numbers">{{estimateDate1}} - {{estimateDate2}}</div>
+                <div class="numbers">{{format-date estimateDate1}} - {{format-date estimateDate2}}</div>
                 <div class="information">{{companies}}</div>
             </div>
             <div class="section3">
@@ -78,11 +73,57 @@
                 <div class="numbers">{{stops}}</div>
                 <div class="information">{{scales}}</div>
             </div>
+            <div class="section5">
+                <%--<div class="numbers">{{#link-to 'flights' tagName='button' class='optionRemove' }}Book{{/link-to}}</div>--%>
+                <button class="optionRemove" {{action "storeFlight" this }}>Remove</button>
+            </div>
         </div>
         {{/each}}
     </div>
 
 </script>
+
+
+<script type="text/x-handlebars" data-template-name="bookedFlights">
+<h2>My Booked Flights</h2>
+    <div class="resultsArea">
+        {{#each}}
+        <div class="result even" >
+            <div class="section1">
+                <div class="price">$ {{price}}</div>
+                <div class="type">{{type}}</div>
+            </div>
+            <div class="section2">
+                <div class="numbers" >{{format-date estimateDate1}} - {{format-date estimateDate2}}</div>
+                <div class="information">{{companies}}</div>
+            </div>
+            <div class="section3">
+                <div class="numbers" >{{estimateTimeTravel}}</div>
+                <div class="information">{{airports}}</div>
+            </div>
+            <div class="section4">
+                <div class="numbers">{{stops}}</div>
+                <div class="information">{{scales}}</div>
+            </div>
+            <div class="section5">
+                <div class="numbers">
+                    <%--{{#link-to 'remove' this tagName='button' class='optionRemove' }}Remove{{/link-to}} --%>
+                    <button class="optionRemove" {{action "removeBookedFlight" this }}>Remove</button>
+
+                </div>
+            </div>
+        </div>
+        {{/each}}
+    </div>
+
+
+</script>
+<script type="text/javascript" src="/resources/js/libs/jquery-1.10.2.js"></script>
+<script type="text/javascript" src="/resources/js/libs/handlebars-1.1.2.js"></script>
+<script type="text/javascript" src="/resources/js/libs/ember-1.4.0.js"></script>
+<script type="text/javascript" src="/resources/js/app.js"></script>
+<script src="/resources/js/libs/jquery-ui/js/jquery-ui-1.10.4.custom.js"></script>
+
 
 </body>
 </html>
--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -2,76 +2,316 @@ App = Ember.Application.create({
     LOG_TRANSITIONS: true
 });
 
+///////////////////////////////////////////////////////// ROUTER ////////////////////////////////////////////////////////
 App.Router.map(function () {
+    this.resource('bookedFlights');
+    this.resource('flights');
+    this.route('search');
+    this.route('remove');
+    this.route('book');
 });
 
-App.FlightsRoute = Ember.Route.extend({
+
+//////////////////////////////////////////////////////// MODELS /////////////////////////////////////////////////////////
+App.SearchParameters = Ember.Object.extend({//useless right now
+    fromV: "",
+    toV: "",
+    date1: "",
+    date2: "",
+    type: "oneWay"
+});
+
+App.SearchResult = Ember.Object.extend({
+    price: "",
+    type: "oneWay",
+    estimateDate1: "",
+    estimateDate2: "",
+    companies: "airlines",
+    estimateTimeTravel: "",
+    airports: "",
+    stops: "",
+    scales: ""
+});
+
+
+/////////////////////////////////////////////////////////// ROUTES //////////////////////////////////////////////////////
+App.ApplicationRoute = Ember.Route.extend({
     model: function () {
-        return App.FLIGHTS;
+        return App.SearchParameters.create();
     }
 });
 
-App.IndexController = Ember.Controller.extend({
-    availableAirports: function () {
-        $.getJSON("/airports").then(function (data) {
-            return data;
+App.IndexRoute = Ember.Route.extend({
+    model: function () {
+        return App.SearchParameters.create();
+    },
+    setupController: function (controller, model) {
+        controller.set("model", model);
+    },
+    actions: {
+        search: function (params) {
+            var query = "/flights?from=";
+            query += "" + params.fromV.substring(0, 3) + "&to=";//add from
+            query += "" + params.toV.substring(0, 3) + "&startDate=";//add to
+            query += "" + params.date1 + "&endDate=";//add startDate
+            query += "" + params.date2 + "&type=oneWay";//add endDate
+            var self = this.controller;
+            console.log('QRY:  ' + query);
+            console.log('date1a:' + this.get('date1'));
+            console.log('date1b:' + this.date1);
+            console.log('date1c:' + this);
+            $.getJSON(query, function (flights) {
+                console.log(flights);
+                App.FLIGHTS = flights; //maybe it must be deleted
+                self.transitionToRoute('search', flights);
+            }, function (error) {
+                return error;
+            });
+        }
+    }
+});
+
+
+App.BookedFlightsRoute = Ember.Route.extend({
+    model: function () {
+        return $.getJSON("/bookedFlights", function (flights) {
+            console.log(flights);
+            return flights;
         });
-    }.property(),
-    startAutocomplete: function () {
-        $("#sourceFlight").autocomplete({
-            source: this.availableAirports,
-            minLength: 3
-        });
-        $("#destinyFlight").autocomplete({
-            source: this.availableAirports,
-            minLength: 3
-        });
+    },
+    actions: {
+        removeBookedFlight: function (params) {
+            var query = "/removeBookedFlight?id=" + params.id;
+            return $.getJSON(query, function (response) {
+                console.log(response);
+                if (response == 'true') {
+                    //TODO do something to remove the element from the model
+                    return $.getJSON("/bookedFlights", function (bookedFlights) {
+                        model = bookedFlights;
+                        return bookedFlights;
+                    });
+                }
+            });
+        }
+    }
+});
+
+
+App.SearchRoute = Ember.Route.extend({
+    model: function () {
+        alert('search route');
+        //return App.FLIGHTS;
+        return App.SearchResult.create();
+    },
+    setupController: function (controller, model) {
+        controller.set("model", model);
+        alert(model);
+    },
+    actions: {
+        search: function () {
+            var query = "/flights?from=";
+            query += "" + model.fromV + "&to=";//add from
+            query += "" + model.toV + "&startDate=";//add to
+            query += "" + model.startDateV + "&endDate=";//add startDate
+            query += "" + model.endDateV + "&type=";//add endDate
+            query += model.typeV + "";//add type
+            return $.getJSON(query).then(function (data) {
+                model = data;
+                return data;
+            });
+        },
+        saveFlight: function (params) {
+            var query = "/saveFlight?price=" + params.price + "&";
+            query += "type=" + params.type + "&";
+            query += "estimatedDate1=" + params.estimateDate1 + "&";
+            query += "estimatedDate2=" + params.estimateDate2 + "&";
+            query += "companies=" + params.companies + "&";
+            query += "estimateTimeTravel=" + params.estimateTimeTravel + "&";
+            query += "airports=" + params.airports + "&";
+            query += "stops=" + params.stops + "&";
+            query += "scales=" + params.scales;
+            console.log(query);
+            return $.getJSON(query, function (response) {
+                if (response == 'true') {
+                    //TODO Add the element to the model
+                    //TODO Redirection to the 'bookedFlights' route
+                }
+                return response;
+            });
+        }
     }
 })
 
+
+/////////////////////////////////////////////////// CONTROLLERS /////////////////////////////////////////////////////////
+App.SearchController = Ember.ArrayController.extend({
+});
+
+
+//////////////////////////////////////////////////////////////////// VIEWS //////////////////////////////////////////////
+/**
+ * view for datepicker in the destiny input html
+ * @type {void|*}
+ */
+App.Date1View = Ember.TextField.extend({
+    tagName: 'input',
+    type: 'text',
+    value: '',
+    visible: true,
+    classNames: ['firstDay'],
+    didInsertElement: function () {
+        this.$().datepicker({ dateFormat: "yy-mm-dd" });
+    }
+});
+
+
+/**
+ * view for autocomplete in the source input html
+ * @type {void|*}
+ */
+App.AutoCompleteSourceView = Ember.TextField.extend({
+    tagName: 'input',
+    type: 'text',
+    value: '',
+    classNames: ['input'],
+    didInsertElement: function () {
+        var availableAirports = function () {
+            return $.getJSON("/airports", function (airports) {
+                console.log(airports);
+                return airports;
+            });
+        };
+        this.$().autocomplete({
+            source: availableAirports,
+            minLength: 3
+        });
+    }
+});
+
+/**
+ * view for autocomplete in the destiny input html
+ * @type {void|*}
+ */
+App.AutoCompleteDestinyView = Ember.TextField.extend({
+    tagName: 'input',
+    type: 'text',
+    value: '',
+    classNames: ['input'],
+    didInsertElement: function () {
+        var availableAirports = function () {
+            return $.getJSON("/airports", function (airports) {
+                console.log(airports);
+                return airports;
+            });
+        };
+        this.$().autocomplete({
+            source: availableAirports,
+            minLength: 3
+        });
+    }
+});
+
+
+////////////////////////////////////////////////////////////////// HELPERS //////////////////////////////////////////////
+/**
+ * Ember helper in order to decorate the dates in the flights lists
+ */
+Ember.Handlebars.helper('format-date', function (date) {
+//    return date.getHours() + ":" + date.getMinutes();
+//    return date.toDateString();
+    return (new Date(date)).toDateString();
+});
+
+
+////////////////////////////////////////////////////////////////  FIXTURES  //////////////////////////////////////////////
+/**
+ * FIXTURES
+ * @type {{price: number, type: string, estimateDate1: Date, estimateDate2: Date, companies: string, estimateTimeTravel: string, airports: string, stops: string, scales: string}[]}
+ */
 App.FLIGHTS = [
     {
-        price: 7,
-        type: "trip",
+        price: 450,
+        type: "oneWay",
         estimateDate1: new Date(),
         estimateDate2: new Date(),
-        companies: "",
-        estimateTimeTravel: "5 hours",
-        airports: "",
-        stops: "",
+        companies: "airlines",
+        estimateTimeTravel: "2H 00M",
+        airports: "airports",
+        stops: "non stop",
         scales: ""
     },
     {
-        price: 8,
-        type: "trip",
+        price: 390,
+        type: "oneWay",
         estimateDate1: new Date(),
         estimateDate2: new Date(),
-        companies: "",
-        estimateTimeTravel: "5 hours",
-        airports: "",
-        stops: "",
+        companies: "airlines",
+        estimateTimeTravel: "4H 30M",
+        airports: "airports",
+        stops: "1 stop",
+        scales: "HMO, PHX"
+    },
+    {
+        price: 400,
+        type: "oneWay",
+        estimateDate1: new Date(),
+        estimateDate2: new Date(),
+        companies: "airlines",
+        estimateTimeTravel: "4H 50M",
+        airports: "airports",
+        stops: "non-stop",
         scales: ""
     },
     {
-        price: 9,
-        type: "trip",
+        price: 300,
+        type: "oneWay",
         estimateDate1: new Date(),
         estimateDate2: new Date(),
-        companies: "",
-        estimateTimeTravel: "5 hours",
-        airports: "",
-        stops: "",
-        scales: ""
-    },
-    {
-        price: 10,
-        type: "trip",
-        estimateDate1: new Date(),
-        estimateDate2: new Date(),
-        companies: "",
-        estimateTimeTravel: "5 hours",
-        airports: "",
-        stops: "",
-        scales: ""
+        companies: "airlines",
+        estimateTimeTravel: "5H 00M",
+        airports: "airports",
+        stops: "2 stops",
+        scales: "HMO, PHX, MEX"
     }
-]
+];
+
+
+/*
+ * -- ApplicationRoute
+ /  ---  index indexRoute - model >> searchParams
+ {{action submitSearh  model}}
+ submitAction : function() {
+ transitionToRoute('flights',searchParams);
+ }
+ /flights   --- resultsRoute - model - lista de resultados
+ init
+ model: function (){
+ return ajax call ('url'+ searchParams.query() )
+ }
+ preModel
+ postModel
+ setupController
+
+
+ ---------------------------
+ IndexController
+ airports: function (){
+ return ajax get airports;
+ ['key-value']
+ }.property();
+
+ ---template index
+ {{type-ahead data=airports name="value" selection=model.from}}
+ {{type-ahead data=airports name="value" selection=model.to}}
+
+
+ -----
+ App.DatePickerView = Ember.View.extend({
+ tagName : 'input'
+
+
+ didInsertElement: function (){
+ this.$().datepicker({ dateFormat: "yy-mm-dd" });
+ }
+ })
+ * */
