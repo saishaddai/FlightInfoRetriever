0abf9a67e75c0cd63b105bd3c3a1af7a6949b0e5
slopez <slopez@nearsoft.com>
2014-04-16 15:15:09 -0400

    modification to the auto complete method to create step by step the criterion to see if we're close to a representative number of results, it is, it will never overfull the screen with a big number of results

--- src/main/java/com/nearsoft/dao/impl/AirportDAOImpl.java
+++ src/main/java/com/nearsoft/dao/impl/AirportDAOImpl.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -45,14 +46,32 @@ public class AirportDAOImpl extends BaseHibernateDAO<Airport, Long> implements A
     }
 
     @Override
-    public List<Airport> autoComplete(String part) {
+    public List<Airport> autoComplete(String part, int maxRows) {
+        List<Airport> airports = new ArrayList<>();
         String prefixSuffix = "%" + part + "%";
         Disjunction or = Restrictions.disjunction();
-        or.add(Restrictions.ilike("name", prefixSuffix, MatchMode.ANYWHERE));
+
         or.add(Restrictions.ilike("iataCode", prefixSuffix, MatchMode.ANYWHERE));
-        or.add(Restrictions.ilike("country", prefixSuffix, MatchMode.ANYWHERE));
-        or.add(Restrictions.ilike("isoCountry", prefixSuffix, MatchMode.ANYWHERE));
+        if ((airports = findByCriteria(true, or)).size() >= maxRows) {
+            return airports;
+        }
+
         or.add(Restrictions.ilike("city", prefixSuffix, MatchMode.ANYWHERE));
+        if ((airports = findByCriteria(true, or)).size() >= maxRows) {
+            return airports;
+        }
+
+        or.add(Restrictions.ilike("country", prefixSuffix, MatchMode.ANYWHERE));
+        if ((airports = findByCriteria(true, or)).size() >= maxRows) {
+            return airports;
+        }
+
+        or.add(Restrictions.ilike("name", prefixSuffix, MatchMode.ANYWHERE));
+        if ((airports = findByCriteria(true, or)).size() >= maxRows) {
+            return airports;
+        }
+
+        or.add(Restrictions.ilike("isoCountry", prefixSuffix, MatchMode.ANYWHERE));
         return findByCriteria(true, or);
     }
 }
