beb0b1ad339be277071715426d5c6ac30f5983dd
slopez <slopez@nearsoft.com>
2014-04-16 15:12:01 -0400

    auto complete relived. Addition of a new parameter MaxRows which lets us have a round number of results

--- src/main/java/com/nearsoft/dao/AirportDAO.java
+++ src/main/java/com/nearsoft/dao/AirportDAO.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface AirportDAO {
 
     Airport findById(Long id);
-    List<Airport> autoComplete(String part);
+
+    List<Airport> autoComplete(String part, int maxRows);
     List<Airport> findAll();
 }
