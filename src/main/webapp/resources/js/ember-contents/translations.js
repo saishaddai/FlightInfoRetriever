60ff470bf6d5da1f2b17931ff0f252cadd59077a
slopez <slopez@nearsoft.com>
2014-04-23 13:21:18 -0400

    translations for i18n

---
+++ src/main/webapp/resources/js/ember-contents/translations.js
@@ -0,0 +1,51 @@
+I18n.translations = {
+    en: {
+        indexTemplate: {
+            search: 'Search',
+            bookedFlights: 'My Flights',
+            go: 'Go',
+            placeholders: {
+                departureAirport: 'Select a departure airport',
+                arrivalAirport: 'Select an arrival airport',
+                departureDate: 'Select a departure date'
+            }
+        },
+        searchTemplate: {
+            flightsFoundCount: {
+                one: 'We found {{count}} flights'
+            },
+            bookedFlightsFoundCount: {
+                one: 'You have {{count}} flights'
+            }
+        },
+        error: {
+            noResults: 'No results found',
+            internalError: 'An unexpected error happened. Please try later'
+        }
+    },
+    es: {
+        indexTemplate: {
+            search: 'Busca',
+            bookedFlights: 'Mis Vuelos',
+            go: 'Ir',
+            placeholders: {
+                departureAirport: 'Elige aeropuerto de salida',
+                arrivalAirport: 'Elige aeropuerto de llegada',
+                departureDate: 'Elige fecha de salida'
+            }
+        },
+        searchTemplate: {
+            flightsFoundCount: {
+                one: 'Encontramos {{count}} vuelos'
+            },
+            bookedFlightsFoundCount: {
+                one: 'Tienes {{count}} vuelos'
+            }
+        },
+        error: {
+            noResults: 'No aparecieron resultados',
+            internalError: 'Error inesperado. Int&eacute;nte m&aacute;s tarde'
+        }
+    }
+};
+
