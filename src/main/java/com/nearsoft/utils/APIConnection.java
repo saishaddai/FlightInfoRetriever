aeeae8010946385778e6005d8484f64559990659
slopez <slopez@nearsoft.com>
2014-04-23 13:25:11 -0400

    catching exceptions

--- src/main/java/com/nearsoft/utils/APIConnection.java
+++ src/main/java/com/nearsoft/utils/APIConnection.java
@@ -4,13 +4,19 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.context.annotation.Scope;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ConnectException;
 import java.util.Map;
 
+@Scope(value = "singleton")
 public class APIConnection {
+
+    public APIConnection() {
+    }
 
     /**
      * Performs API calls through a internet HTTP client
@@ -20,7 +26,7 @@ public class APIConnection {
      * @return an object containing the content in the URL
      * @throws ConnectException if connection failed
      */
-    public static String callAPI(String url, Map<String, String> headers) throws ConnectException {
+    public String callAPI(String url, Map<String, String> headers) throws ConnectException {
         if (url == null) {
             throw new ConnectException("invalid URL");
         }
@@ -36,14 +42,18 @@ public class APIConnection {
         HttpResponse response;
         try {
             response = client.execute(request);
+        } catch (IOException e) {
+            throw new ConnectException("Unable to connect with the url: " + url);
+        }
+        try {
             // Get the response
             BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
             String line;
             while ((line = rd.readLine()) != null) {
                 textView.append(line);
             }
-        } catch (Exception e) {
-            throw new ConnectException();
+        } catch (IOException e) {
+            throw new ConnectException("There was an error processing the server answer");
         }
         return textView.toString();
 
