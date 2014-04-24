03307acf0f70a149a1284d8263d13bfc8d856e52
slopez <slopez@nearsoft.com>
2014-04-24 12:26:23 -0400

    extra code and useless commentaries removed

--- src/main/java/com/nearsoft/utils/APIConnection.java
+++ src/main/java/com/nearsoft/utils/APIConnection.java
@@ -4,7 +4,6 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.springframework.context.annotation.Scope;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,20 +11,11 @@ import java.io.InputStreamReader;
 import java.net.ConnectException;
 import java.util.Map;
 
-@Scope(value = "singleton")
 public class APIConnection {
 
     public APIConnection() {
     }
 
-    /**
-     * Performs API calls through a internet HTTP client
-     *
-     * @param url     the URL to connect to
-     * @param headers optionals headers in the request message
-     * @return an object containing the content in the URL
-     * @throws ConnectException if connection failed
-     */
     public String callAPI(String url, Map<String, String> headers) throws ConnectException {
         if (url == null) {
             throw new ConnectException("invalid URL");
@@ -46,7 +36,6 @@ public class APIConnection {
             throw new ConnectException("Unable to connect with the url: " + url);
         }
         try {
-            // Get the response
             BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
             String line;
             while ((line = rd.readLine()) != null) {
