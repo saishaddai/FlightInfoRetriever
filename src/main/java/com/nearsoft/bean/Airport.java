4c165dc4f11676debe559dbae9feead11a1a3372
slopez <slopez@nearsoft.com>
2014-04-24 11:26:01 -0400

    unnecessary methods removed

--- src/main/java/com/nearsoft/bean/Airport.java
+++ src/main/java/com/nearsoft/bean/Airport.java
@@ -1,57 +1,51 @@
 package com.nearsoft.bean;
 
-import com.sun.org.apache.regexp.internal.recompile;
-import org.hibernate.annotations.*;
 import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.validator.constraints.NotBlank;
 
 import javax.persistence.*;
-import javax.persistence.Entity;
-import javax.persistence.Table;
 import java.io.Serializable;
 
 /**
- * Created by slopez on 2/27/14.
+ * Created by Saidel Lopez on 2/27/14.
+ * Airport mapping for auto complete feature
  */
 @Entity
-@Table(name="airports")
+@Table(name = "airports")
 @Cacheable
 @Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
 public class Airport implements Serializable {
 
     @Id
     @GeneratedValue
-    private Long id ;
+    private Long id;
 
-    @NotBlank @Column(updatable=false)
+    @NotBlank
+    @Column(updatable = false)
     private String name;
 
-    @NotBlank @Column(updatable=false)
+    @NotBlank
+    @Column(updatable = false)
     private String iataCode;
 
-    @NotBlank @Column(updatable=false)
+    @NotBlank
+    @Column(updatable = false)
     private String country;
 
-    @NotBlank @Column(updatable=false)
-    private String isoCountry;
-
-    @NotBlank @Column(updatable=false)
+    @NotBlank
+    @Column(updatable = false)
     private String city;
 
-    public Airport(){}
+    public Airport() {
+    }
 
-    public Airport(Long id, String name, String iataCode, String country, String isoCountry, String city) {
+    public Airport(Long id, String name, String iataCode, String country, String city) {
         this.setId(id);
         this.setName(name);
         this.setIataCode(iataCode);
         this.setCountry(country);
-        this.setIsoCountry(isoCountry);
         this.setCity(city);
-    }
-
-
-    public Long getId() {
-        return id;
     }
 
     public void setId(Long id) {
@@ -66,28 +60,12 @@ public class Airport implements Serializable {
         this.name = name;
     }
 
-    public String getIataCode() {
-        return iataCode;
-    }
-
     public void setIataCode(String iataCode) {
         this.iataCode = iataCode;
     }
 
-    public String getCountry() {
-        return country;
-    }
-
     public void setCountry(String country) {
         this.country = country;
-    }
-
-    public String getIsoCountry() {
-        return isoCountry;
-    }
-
-    public void setIsoCountry(String isoCountry) {
-        this.isoCountry = isoCountry;
     }
 
     public String getCity() {
@@ -98,47 +76,9 @@ public class Airport implements Serializable {
         this.city = city;
     }
 
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Airport)) return false;
-
-        Airport airport = (Airport) o;
-
-        if (!city.equals(airport.city)) return false;
-        if (!country.equals(airport.country)) return false;
-        if (iataCode != null ? !iataCode.equals(airport.iataCode) : airport.iataCode != null) return false;
-        if (!id.equals(airport.id)) return false;
-        if (!isoCountry.equals(airport.isoCountry)) return false;
-        if (!name.equals(airport.name)) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id.hashCode();
-        result = 31 * result + name.hashCode();
-        result = 31 * result + (iataCode != null ? iataCode.hashCode() : 0);
-        result = 31 * result + country.hashCode();
-        result = 31 * result + isoCountry.hashCode();
-        result = 31 * result + city.hashCode();
-        return result;
-    }
-
     @Override
     public String toString() {
-        //"CUL - Federal de Bachigualato International Airport - Culiacan, Mexico";
         return this.iataCode + " - " + this.name + " - " + this.city + ", " + this.country;
-//        return "Airport{" +
-//                "id=" + id +
-//                ", name='" + name + '\'' +
-//                ", iataCode='" + iataCode + '\'' +
-//                ", country='" + country + '\'' +
-//                ", isoCountry='" + isoCountry + '\'' +
-//                ", city='" + city + '\'' +
-//                '}';
     }
 }
 
--- src/main/java/com/nearsoft/bean/Flight.java
+++ src/main/java/com/nearsoft/bean/Flight.java
@@ -7,62 +7,70 @@ import javax.persistence.*;
 import java.io.Serializable;
 
 /**
- * Created by slopez on 2/27/14.
+ * Created by Saidel Lopez on 2/27/14
+ * Representation of either a search result and a booked flight.
  */
 @Entity
-@Table(name="flights")
+@Table(name = "flights")
 public class Flight implements Serializable {
 
-    //@JsonIgnore
     @Id
     @GeneratedValue
     private Long id;
 
-    @NotBlank @Column(updatable=false)
+    @NotBlank
+    @Column(updatable = false)
     private String price;
 
-    @NotBlank @Column(updatable=false)
+    @NotBlank
+    @Column(updatable = false)
     private String type;
-    //@JsonProperty("est") if change the name of the attribute is needed
-    @NotBlank @Column(updatable=false)
+    @NotBlank
+    @Column(updatable = false)
     private String estimateDate1;
-    @NotBlank @Column(updatable=false)
+    @NotBlank
+    @Column(updatable = false)
     private String estimateDate2;
-    @NotBlank @Column(updatable=false)
+    @NotBlank
+    @Column(updatable = false)
     private String companies;
-    @NotBlank @Column(updatable=false)
+    @NotBlank
+    @Column(updatable = false)
     private String estimateTimeTravel;
-    @NotBlank @Column(updatable=false)
+    @NotBlank
+    @Column(updatable = false)
     private String airports;
     @Column(updatable = false)
     private String stops;
     @Column(updatable = false)
     private String scales;
     @Column(updatable = true)
-    private boolean booked=false;
+    private boolean booked = false;
 
     /**
      * Sole constructor
      */
-    public Flight(){}
+    public Flight() {
+    }
 
     /**
      * Creates an instance of a flight object
-     * @param id unique identifier for a flight
-     * @param price the price of the flight commonly on dollars
-     * @param type The type of the flight (one Way or round trip)
-     * @param estimateDate1 the date and hour of departure
-     * @param estimateDate2 the date and hour of arrival (in a round trip this represents the departure time of return flight)
-     * @param companies the airlines involved in the flight
+     *
+     * @param id                 unique identifier for a flight
+     * @param price              the price of the flight commonly on dollars
+     * @param type               The type of the flight (one Way or round trip)
+     * @param estimateDate1      the date and hour of departure
+     * @param estimateDate2      the date and hour of arrival (in a round trip this represents the departure time of return flight)
+     * @param companies          the airlines involved in the flight
      * @param estimateTimeTravel the estimated time travel
-     * @param airports the airports involved in the flight (departure, arrival, scales)
-     * @param stops the number of stops/scales to take in the journey
-     * @param scales a description of the stops as convenient of the API interest
-     * @param booked true if the flights has benn taken for an user
+     * @param airports           the airports involved in the flight (departure, arrival, scales)
+     * @param stops              the number of stops/scales to take in the journey
+     * @param scales             a description of the stops as convenient of the API interest
+     * @param booked             true if the flights has benn taken for an user
      */
     public Flight(Long id, String price, String type, String estimateDate1, String estimateDate2, String companies,
                   String estimateTimeTravel, String airports, String stops, String scales, boolean booked) {
-        this.id= id;
+        this.id = id;
         this.price = price;
         this.type = type;
         this.estimateDate1 = estimateDate1;
@@ -80,122 +88,45 @@ public class Flight implements Serializable {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getPrice() {
         return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
     }
 
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
-    }
-
     public String getEstimateDate1() {
         return estimateDate1;
-    }
-
-    public void setEstimateDate1(String estimateDate1) {
-        this.estimateDate1 = estimateDate1;
     }
 
     public String getEstimateDate2() {
         return estimateDate2;
     }
 
-    public void setEstimateDate2(String estimateDate2) {
-        this.estimateDate2 = estimateDate2;
-    }
-
     public String getCompanies() {
         return companies;
-    }
-
-    public void setCompanies(String companies) {
-        this.companies = companies;
     }
 
     public String getEstimateTimeTravel() {
         return estimateTimeTravel;
     }
 
-    public void setEstimateTimeTravel(String estimateTimeTravel) {
-        this.estimateTimeTravel = estimateTimeTravel;
-    }
-
     public String getAirports() {
         return airports;
     }
 
-    public void setAirports(String airports) {
-        this.airports = airports;
-    }
 
     public String getStops() {
         return stops;
-    }
-
-    public void setStops(String stops) {
-        this.stops = stops;
     }
 
     public String getScales() {
         return scales;
     }
 
-    public void setScales(String scales) {
-        this.scales = scales;
-    }
-
     public boolean isBooked() {
         return booked;
     }
 
-    public void setBooked(boolean booked) {
-        this.booked = booked;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Flight flight = (Flight) o;
-
-        if (!airports.equals(flight.airports)) return false;
-        if (!companies.equals(flight.companies)) return false;
-        if (!estimateDate1.equals(flight.estimateDate1)) return false;
-        if (!estimateDate2.equals(flight.estimateDate2)) return false;
-        if (!estimateTimeTravel.equals(flight.estimateTimeTravel)) return false;
-        if (!id.equals(flight.id)) return false;
-        if (!price.equals(flight.price)) return false;
-        if (!scales.equals(flight.scales)) return false;
-        if (!stops.equals(flight.stops)) return false;
-        if (!type.equals(flight.type)) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id.hashCode();
-        result = 31 * result + price.hashCode();
-        result = 31 * result + type.hashCode();
-        result = 31 * result + estimateDate1.hashCode();
-        result = 31 * result + estimateDate2.hashCode();
-        result = 31 * result + companies.hashCode();
-        result = 31 * result + estimateTimeTravel.hashCode();
-        result = 31 * result + airports.hashCode();
-        result = 31 * result + stops.hashCode();
-        return result;
-    }
 }
