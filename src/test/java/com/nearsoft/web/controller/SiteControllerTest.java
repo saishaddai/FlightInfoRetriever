9817fd3e72e6fd9f92f016f7920724d837b7febc
slopez <slopez@nearsoft.com>
2014-04-25 09:50:27 -0400

    removed a System.out.println

--- src/test/java/com/nearsoft/web/controller/SiteControllerTest.java
+++ src/test/java/com/nearsoft/web/controller/SiteControllerTest.java
@@ -247,7 +247,6 @@ public class SiteControllerTest {
                 .andExpect(status().isOk()).andReturn().getResponse();
 
         String responseAsString = response.getContentAsString();
-        System.out.println(responseAsString);
         assertTrue(response.getContentType().equalsIgnoreCase("application/json;charset=UTF-8")); //which proves is a JSON
         assertTrue(responseAsString.equals("true"));//to prove that is a JSON and is true string
     }
