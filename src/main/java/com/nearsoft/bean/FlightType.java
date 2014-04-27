6aadb6f58581fdcb0972859dee42788818ef7ecc
slopez <slopez@nearsoft.com>
2014-04-27 19:10:04 -0400

    flight type, without useless code

--- src/main/java/com/nearsoft/bean/FlightType.java
+++ src/main/java/com/nearsoft/bean/FlightType.java
@@ -4,18 +4,6 @@ package com.nearsoft.bean;
  * Created by Saidel Lopez on 25/04/14.
  */
 public enum FlightType {
-    ONE_WAY("oneWay"),
-    ROUND_TRIP("roundTrip");
-
-    private String validFlightType;
-
-    FlightType(String flightType) {
-        validFlightType = flightType;
-    }
-
-    public String getFlightType() {
-        return validFlightType;
-    }
-
-
+    ONE_WAY,
+    ROUND_TRIP;
 }
